b6b5ca79a5bfdee89550da82952851e596734433
lovak_000 <lovak_000@Koval>
2021-02-07 17:36:36 +0200

    added new projeck to github

---
+++ lesson-06/src/main/java/mapper/BucketMapper.java
@@ -0,0 +1,20 @@
+package mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import domain.Bucket;
+
+public class BucketMapper {
+
+	public static Bucket map(ResultSet result) throws SQLException {
+
+		Integer bucketId = result.getInt("id");
+		Integer userId = result.getInt("user_id");
+		Integer magazineId = result.getInt("magazine_id");
+		java.util.Date date = result.getDate("date");
+
+		return new Bucket(bucketId, userId, magazineId, date);
+	}
+
+}
